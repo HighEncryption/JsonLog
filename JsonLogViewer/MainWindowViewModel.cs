bec681a3d559f8b84cfdc2fbb807a8d93b8cb7e0
HighEncryption <29420483+HighEncryption@users.noreply.github.com>
2017-12-16 00:48:56 -0800

    Moving JsonLogViewer to TraceEvent (ETW)

--- JsonLogViewer/MainWindowViewModel.cs
+++ JsonLogViewer/MainWindowViewModel.cs
@@ -12,12 +12,60 @@ namespace JsonLogViewer
 
     using JsonLogViewer.Framework;
 
+    using Microsoft.Diagnostics.Tracing;
+    using Microsoft.Diagnostics.Tracing.Session;
+
     public class MainWindowViewModel : ViewModelBase, IDisposable
     {
         private PipeWatcher pipeWatcher;
         private LogWatcher logWatcher;
+        private TraceEventSession listener;
 
         public ObservableCollection<LogEntry> Entries { get; }
+
+
+        private void DynamicOnAll(TraceEvent traceEvent)
+        {
+            if (traceEvent.EventName == "ManifestData")
+            {
+                return;
+            }
+
+            App.DispatcherInvoke(() =>
+            {
+                this.Entries.Add(new LogEntry()
+                {
+                    Message = traceEvent.FormattedMessage,
+                    Timestamp = traceEvent.TimeStamp,
+                    Level = TraceEventLevelConverter(traceEvent.Level),
+                    ThreadId = traceEvent.ThreadID
+                });
+
+                CollectionViewSource.GetDefaultView(this.Entries).MoveCurrentToLast();
+            });
+        }
+
+        private static string TraceEventLevelConverter(TraceEventLevel level)
+        {
+            switch (level)
+            {
+                case TraceEventLevel.Always:
+                    return "ALL";
+                case TraceEventLevel.Critical:
+                    return "CRIT";
+                case TraceEventLevel.Error:
+                    return "ERROR";
+                case TraceEventLevel.Warning:
+                    return "WARN";
+                case TraceEventLevel.Informational:
+                    return "INFO";
+                case TraceEventLevel.Verbose:
+                    return "VERB";
+                default:
+                    return "????";
+            }
+        }
+
 
         public MainWindowViewModel()
         {
@@ -26,6 +74,16 @@ namespace JsonLogViewer
             string[] cmdArgs = Environment.GetCommandLineArgs();
             Dictionary<string, string> commandLineArgs = new Dictionary<string, string>();
 
+            this.listener = new TraceEventSession("MyViewerSession");
+            this.listener.Source.Dynamic.All += DynamicOnAll;
+
+            var eventSourceGuid = TraceEventProviders.GetEventSourceGuidFromName(
+                "SyncPro-Tracing"); // Get the unique ID for the eventSouce. 
+            this.listener.EnableProvider(eventSourceGuid);
+
+            Task.Factory.StartNew(() => { this.listener.Source.Process(); });
+
+            /*
             if (cmdArgs.Length == 2)
             {
                 try
@@ -108,6 +166,7 @@ namespace JsonLogViewer
                 };
                 this.logWatcher.Start();
             }
+            */
         }
 
         public void Dispose()
@@ -123,6 +182,20 @@ namespace JsonLogViewer
                 this.logWatcher.Dispose();
                 this.logWatcher = null;
             }
+
+            if (this.listener != null)
+            {
+                this.listener.Dispose();
+                this.listener = null;
+            }
+        }
+    }
+
+    public class CustomListener
+    {
+        public void Init()
+        {
+            
         }
     }
 }
